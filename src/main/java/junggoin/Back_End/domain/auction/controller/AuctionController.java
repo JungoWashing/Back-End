5aca5811e91210da27be997d69d57f700c63402b
jewon <96jwjw@naver.com>
2024-11-25 21:08:43 +0900

    refactor: BidController 메서드를 AuctionController로 이동

--- src/main/java/junggoin/Back_End/domain/auction/controller/AuctionController.java
+++ src/main/java/junggoin/Back_End/domain/auction/controller/AuctionController.java
@@ -1,11 +1,14 @@
 package junggoin.Back_End.domain.auction.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import junggoin.Back_End.domain.auction.Auction;
 import junggoin.Back_End.domain.auction.dto.ProductRepDto;
 import junggoin.Back_End.domain.auction.dto.ProductReqDto;
 import junggoin.Back_End.domain.auction.service.AuctionService;
+import junggoin.Back_End.domain.bid.dto.BidRequestDto;
+import junggoin.Back_End.domain.bid.dto.BidResponseDto;
 import junggoin.Back_End.domain.bid.service.BidService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -48,6 +51,13 @@ public class AuctionController {
         Long auctionId = auctionService.deleteAuction(id);
 
         return ResponseEntity.ok("경매 삭제 성공: "+auctionId);
+    }
+
+    // 경매 입찰
+    @PostMapping("/{id}/bids")
+    public ResponseEntity<BidResponseDto> bidAuction(@PathVariable Long id, @RequestBody
+    BidRequestDto bidRequestDto) throws InterruptedException, TimeoutException {
+        return ResponseEntity.ok(bidService.bidAuction(id,bidRequestDto)) ;
     }
 
     // 판매 내역 조회
